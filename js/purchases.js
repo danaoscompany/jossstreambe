cc7e2c8f1bfdc4309a16dcddc9c28fea0cb8897f
Dana Prakoso <danaossoftware@gmail.com>
2019-02-20 23:50:21 +0700

    commit

--- js/purchases.js
+++ js/purchases.js
@@ -1,9 +1,7 @@
 var purchases;
 
 $(document).ready(function() {
-    console.log("Document is ready");
-    show("Document is ready");
-    //getPurchases();
+    getPurchases();
 });
 
 function getPurchases() {
