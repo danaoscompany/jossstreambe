5e7db7f89b70968ada6d88ee192e28da1cc0446c
Dana Prakoso <danaossoftware@gmail.com>
2019-03-02 22:38:29 +0700

    commit

--- js/channels.js
+++ js/channels.js
@@ -2,11 +2,11 @@ var m3uData;
 var channels = [];
 
 $(document).ready(function() {
-    if (location.protocol != 'https:') {
-        location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-    } else {
-        getChannels();
+    if (location.protocol !== "https:") {
+        location.protocol = "https:";
+        return;
     }
+    getChannels();
 });
 
 function getChannels() {
