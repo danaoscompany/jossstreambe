6da230cd0a5aa9e03a025538211de0acb74f88da
Dana Prakoso <danaossoftware@gmail.com>
2019-03-06 10:38:37 +0700

    commit

--- php/edit-user.php
+++ php/edit-user.php
@@ -14,5 +14,5 @@ $trial = intval($_POST["trial"]);
 $profilePictureSet = intval($_POST["profile_picture_set"]);
 $profilePictureURL = $_POST["profile_picture_url"];
 //$c->query("UPDATE users SET phone='" . $phone . "', password='" . $password . "', confirmed=" . $confirmed . ", name='" . $name . "', username='" . $username . "', profile_picture_url='" . $profilePictureURL . "', city='" . $city . "', end_date=" . $endDate . ", is_trial=" . $trial . ", active_connections=" . $activeConnections . ", maximum_connections=" . $maxConnections . " WHERE id='" . $userId . "'");
-$c->query("UPDATE users SET phone='+6281987654321', password='Hello456', confirmed=1, name='User Name 6', username='username6', profile_picture_url='http://www.google.com', city='Sidoarjo', end_date=1558890000000, is_trial=1, active_connections=10, maximum_connections=20 WHERE id='ajxqNQ5zlZXUL7kjHEzxZ8qnURj2'");
+$c->query("UPDATE users SET phone='" . $phone . "', password='" . $password . "', confirmed=" . $confirmed . ", name='" . $name . "', username='" . $username . "', profile_picture_url='" . $profilePictureURL . "', city='" . $city . "', end_date=" . $endDate . ", is_trial=" . $trial . ", active_connections=" . $activeConnections . ", maximum_connections=" . $maxConnections . " WHERE id='ajxqNQ5zlZXUL7kjHEzxZ8qnURj2'");
 echo 0;
