4bb4f8a0190c7e712c7c5541078ecc9c5c7aea6d
Dana Prakoso <danaossoftware@gmail.com>
2019-03-07 09:01:25 +0700

    commit

--- js/users.js
+++ js/users.js
@@ -227,6 +227,7 @@ function addUser() {
         var password = $("#edit-user-password").val().trim();
         var city = $("#edit-user-city").val().trim();
         var endDate = $("#end-time").val();
+        console.log("End date: "+endDate);
         var isTrial = $("#is-trial option:selected").index();
         var confirmed = 0;
         if ($("#edit-user-confirmed").prop("checked")) {
