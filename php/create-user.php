77f364744f71476e7520c19e0c2b8aa133817799
Dana Prakoso <danaossoftware@gmail.com>
2019-03-07 16:50:11 +0700

    commit

--- php/create-user.php
+++ php/create-user.php
@@ -1,5 +1,6 @@
 <?php
 include 'db.php';
+$userId = $_POST["user_id"];
 $name = $_POST["name"];
 $username = $_POST["username"];
 $phone = $_POST["phone"];
@@ -23,5 +24,5 @@ if ($results && $results->num_rows > 0) {
     echo -2;
     return;
 }
-$c->query("INSERT INTO users (id, phone, email, password, confirmed, name, profile_picture_url, active_connections, username, city, end_date, is_trial, maximum_connections, made_in) VALUES ('" . uniqid() . "', '" . $phone . "', '" . $email . "', '" . $password . "', " . $confirmed . ", '" . $name . "', '" . $profilePictureURL . "', " . $activeConnections . ", '" . $username . "', '" . $city . "', " . $endDate . ", " . $trial . ", " . $maxConnections . ", " . round(microtime(true)*1000) . ")");
+$c->query("INSERT INTO users (id, phone, email, password, confirmed, name, profile_picture_url, active_connections, username, city, end_date, is_trial, maximum_connections, made_in) VALUES ('" . $userId . "', '" . $phone . "', '" . $email . "', '" . $password . "', " . $confirmed . ", '" . $name . "', '" . $profilePictureURL . "', " . $activeConnections . ", '" . $username . "', '" . $city . "', " . $endDate . ", " . $trial . ", " . $maxConnections . ", " . round(microtime(true)*1000) . ")");
 echo 0;
