bfde6b89559d2629265dae2b504fccce1a54085e
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-11-04 22:08:23 +0700

    commit

--- js/notifications.js
+++ js/notifications.js
@@ -32,11 +32,15 @@ function getNotifications() {
                 dateText += monthNames[date.getMonth()];
                 dateText += " ";
                 dateText += date.getFullYear();
+                var content = notification["content"];
+                if (content.getLength() > 30) {
+                    content = content.substring(0, 29);
+                }
                 $("#notifications").append("" +
                     "<tr>" +
-                    "<td><div style='background-color: #2f2e4d; width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; color: white;'>" + i + "</div></td>" +
+                    "<td><div style='background-color: #2f2e4d; width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; color: white;'>" + (i+1) + "</div></td>" +
                     "<td>" + notification["title"] + "</td>" +
-                    "<td>" + notification["content"] + "</td>" +
+                    "<td>" + content + "</td>" +
                     "<td>" + dateText + "</td>" +
                     "<td><a class='edit-notification link'>Ubah</a></td>" +
                     "<td><a class='delete-notification link'>Hapus</a></td>" +
